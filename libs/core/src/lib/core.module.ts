84554cbd978a10b196594f4a394b40b759dfd9c7
guido offermans <guidooffermans@hotmail.com>
2021-11-06 15:07:55 +0100

    add config and graphql

--- libs/core/src/lib/core.module.ts
+++ libs/core/src/lib/core.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+
+import { configuration } from './config/configuration';
+import { validationSchema } from './config/validation';
+import { CoreResolver } from './core.resolver';
 
 @Module({
-	controllers: [],
-	providers: [],
-	exports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      validationSchema,
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      sortSchema: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+  ],
+  providers: [CoreResolver],
+  exports: [],
 })
-export class CoreModule {}
+export class CoreModule {
+}
