23e72bd9e70e5adf6277120b3ccbc0ab22952ceb
guido offermans <guidooffermans@hotmail.com>
2021-11-06 14:49:22 +0100

    add nest api and lib

---
+++ apps/api/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { CoreModule } from '@lost-cities/core';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [CoreModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+}
